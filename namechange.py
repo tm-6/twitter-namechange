611d67b681a314dc6a36505ec2e22bda374c1f53
tomoki uezu <square.t.u68@gmail.com>
2022-01-05 00:26:36 +0900

    1st commit

---
+++ namechange.py
@@ -0,0 +1,21 @@
+import json
+import tweepy
+from datetime import datetime
+
+with open('secret.json') as f:
+    secret = json.load(f)
+
+auth = tweepy.OAuthHandler(secret['API_Key'], secret['API_Key_Secret'])
+auth.set_access_token(secret['Access_Token'], secret['Access_Token_Secret'])
+api = tweepy.API(auth)
+
+def name_change():
+    end_day = datetime(2022,6,8)
+    today = datetime.now()
+
+    day = end_day - today
+    day = day.days + 1
+
+    api.update_profile(name = f'うえとも@{day}日後までにエンジニア転職する')
+
+name_change()
