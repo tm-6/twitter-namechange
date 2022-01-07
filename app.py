1a14d0861c48ff3b2f00ebe9bcf3fbda7b12df56
tomoki uezu <square.t.u68@gmail.com>
2022-01-07 21:24:54 +0900

    app.py変更
    名前を変更

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def name_change():
     day = end_day - today
     day = day.days # + 1 HerokuがUTCしか選択できないので + 1 は無し
 
-    api.update_profile(name = f'うえとも@{day}日後までにエンジニアに転職！')
+    api.update_profile(name = f'うえとも@{day}日後までにエンジニア転職！')
 
 app = name_change()
 
